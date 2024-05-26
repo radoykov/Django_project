eb629097b2b239a80a9bc259223069f749062fbb
radoykov <alex.radoykov@gmail.com>
2024-05-26 20:51:37 +0300

    Added vacation details view

--- vacations_sharing/vacations/urls.py
+++ vacations_sharing/vacations/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('vacations/', views.vacations_list, name="vacations_list"),
+    path('vacations/<int:pk>/', views.vacation_detail, name="vacation_detail"),
+    
     
     path('categories/', views.categories_list, name="categories_list"),
     path('categories/<int:pk>/', views.category_detail, name="category_detail"),
--- vacations_sharing/vacations/views.py
+++ vacations_sharing/vacations/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 # from django.http import HttpResponse
-from django.db.models.deletion import RestrictedError, ProtectedError
+from django.db.models.deletion import RestrictedError
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
@@ -9,13 +9,20 @@ from .serializers import VacationSerializer, CategorySerializer
 from .models import Vacation, Category
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def vacations_list(request):
 
     if request.method == 'GET':
         vacations = Vacation.objects.select_related('category', 'author')
         serializer = VacationSerializer(vacations, many=True)
         return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = VacationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
 @api_view(['GET', 'POST'])
 def categories_list(request):
@@ -62,4 +69,35 @@ def category_detail(request, pk):
             ]
             response_data = {"detail" : str(error), "restricted_elements": restricted_elements}
             return Response(data=response_data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def vacation_detail(request, pk):
+
+    try:
+        vacation = Vacation.objects.get(pk=pk)
+    except Vacation.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = VacationSerializer(vacation)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = VacationSerializer(vacation, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'PATCH':
+        serializer = VacationSerializer(vacation, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        vacation.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
